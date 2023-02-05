90f40e8d6b7d86c03a1ecc28884f5fae56ef3737
Bryan Joseph <bryanllana14@gmail.com>
2023-02-05 18:29:32 -0500

    Estilos listos

--- helpers/mostrarTabla.js
+++ helpers/mostrarTabla.js
@@ -1,25 +1,26 @@
 const fs = require('fs')
+require('colors')
 
 const mostrarTabla = async(base, limite, mostrar) => {
     try {
         let consolaTabla = '', dataTabla = ''
     
         for(let i=1; i<=limite; i++){
-            consolaTabla += `${base} x ${i} = ${base*i} \n`
+            consolaTabla += `${String(base).blue} ${'x'.red} ${String(i).blue} ${'='.red} ${String(base*i).blue} \n`
             dataTabla += `${base} x ${i} = ${base*i} \n`
         }
 
         if(mostrar){
-            console.log(`===================`)
-            console.log(`=== Tabla del ${base} ===`)
-            console.log(`===================`)
+            console.log(`===================`.green)
+            console.log(`=== ${'Tabla del'.bgRed} ${String(base).bgRed} ===`.green)
+            console.log(`===================`.green)
         
             console.log(consolaTabla)
         }
     
         fs.writeFileSync(`./tablas/tabla-del-${base}.txt`, dataTabla);
     
-        return `Archivo tabla-del-${base}.txt creado`
+        return `Archivo tabla-del-${base}.txt creado`.rainbow
     } catch (error) {
         throw new error
     }
